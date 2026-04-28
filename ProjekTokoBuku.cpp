07bc6c1c3b17aece6e124ee46936217857db5c55
Wedneszay <mazayafirasyan@gmail.com>
2026-04-28 21:19:09 +0700

    Menambahkan initial load untuk file data buku

--- ProjekTokoBuku.cpp
+++ ProjekTokoBuku.cpp
@@ -1,24 +1,47 @@
 #include <iostream>
 #include <iomanip>
 #include <stdio.h>
-#include <string>
+#include <cstring>
 using namespace std;
+
+// Struct buku
+typedef struct Buku {
+    char isbn[20];
+    char judul[100];
+    char penulis[50];
+    int stok;
+    double harga;
+
+    // Pointer untuk double linked list
+    struct Buku *next; 
+    struct Buku *prev;
+} DataBuku;
+DataBuku *head = nullptr;
+DataBuku *tail = nullptr;
+
+// List fungsi
+void admin();
+void lihatKatalogBuku();
+void loadFileBuku();
 
 int main()
 {
     int opsiRole;
     bool pilihRole = false;
-    cout << "Selamat datang di Program Manajemen Toko Buku!" << endl;
-    cout << "Silakan pilih role anda" << endl;
+
+    loadFileBuku();
+
+    cout << "PROGRAM MANAJEMEN TOKO BUKU" << endl;
+    cout << "MENU ROLE" << endl; // Memilih role
     cout << "1. Admin" << endl;
     cout << "2. Kasir" << endl;
-    while (!pilihRole) {
-        cout << ">> ";
-        cin >> opsiRole;
+    while (!pilihRole) { // Perulangan jika input tidak valid
+        cout << "Silahkan pilih role anda: "; cin >> opsiRole;
         switch (opsiRole) {
         case 1:
             cout << "Anda adalah admin" << endl;
             pilihRole = true;
+            admin();
             break;
         case 2:
             cout << "Anda adalah kasir" << endl;
@@ -30,3 +53,54 @@ int main()
     }
     return 0;
 }
+
+void loadFileBuku() {
+    FILE *fptr;
+    fptr = fopen ("databuku.txt", "r");
+    if (fptr == nullptr) {
+        cout << "Database tidak ditemukan, memulai dengan katalog kosong" << endl;
+        return;
+    }
+
+    char isbn[20], judul[100], penulis[50];
+    int stok;
+    double harga;
+
+    while (fscanf(fptr, "%[^;];%[^;];%[^;];%d;%lf\n", isbn, judul, penulis, &stok, &harga)) {
+        //masukkan fungsi tambah linked list
+    }
+    
+    fclose(fptr);
+
+}
+
+void admin() {
+    //system("cls");
+    int opsiAdmin;
+    bool pilihAdmin = false;
+    cout << "MENU ADMIN" << endl;
+    cout << "1. Lihat Katalog" << endl;
+    cout << "2. Tambah Buku" << endl;
+    cout << "3. Hapus Buku" << endl;
+    cout << "4. Update Stok Buku" << endl;
+    cout << "Pilih menu: "; cin >> opsiAdmin;
+
+    while (!pilihAdmin) {
+        switch (opsiAdmin) {
+        case 1:
+            pilihAdmin = true;
+            
+            break;
+        default:
+            break;
+        }
+    }
+    
+}
+
+void lihatKatalogBuku() {
+    DataBuku *temp = head;
+
+}
+
+
